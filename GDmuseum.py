e12ea01b820487924f3cdc377c88f385c78e66ca
mmuu1987 <262663941@qq.com>
2020-08-04 19:23:13 +0800

    update

--- GDmuseum.py
+++ GDmuseum.py
@@ -4,9 +4,11 @@ from typing import TextIO
 import requests
 import re
 import os
+import time
 from threading import Timer
 from bs4 import BeautifulSoup as bs
 from bs4.element import NavigableString
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 host = 'http://www.gdmuseum.com/'
@@ -28,10 +30,26 @@ Secondir = ''
 headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Mobile Safari/537.36'}
 
+# 线程锁
+islock = 0
+
+
+# 线程安全
+def LockThreadGetURL(url):
+    while not islock:
+        print("等待解锁")
+        time.sleep(1)
+    print("已经解锁，利用代理获取URL")
+    res = requests.get(url, headers=headers, proxies=proxies)
+    return res
+
 
 # 定时获取代理
 def TimeGetproxies():
     # 获取API接口返回的代理IP
+    global islock
+    print("线程加锁")
+    islock = 0
     proxy_ip = requests.get(api_url).text
     global proxies
     proxies = {
@@ -39,6 +57,8 @@ def TimeGetproxies():
         "https": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": proxy_ip}
     }
     print("定时获取IP代理========================>>>>>>>>>>>>>>>>>>>", proxies)
+    islock = 1
+    print("线程解锁")
     Timer(120, TimeGetproxies).start()
 
 
@@ -48,7 +68,7 @@ def safe_string(_string):
 
 def DownImage(image_url) -> None:
     try:
-        image_res = requests.get(image_url, proxies=proxies)
+        image_res = LockThreadGetURL(image_url)
         string_str = image_res.text
         temp = re.findall(".*?'src':'(.*?\.jpg)','title'", string_str)
         image_url = f'{host}{temp[0]}'
@@ -63,15 +83,13 @@ def DownImage(image_url) -> None:
             detect += desire.text.replace("\r", "").replace("\n", "").strip() + '\r\n'
         # 拿取详情信息
         detail = soup.select('.detail_cont')[0].select(' .cont')[0]
-        if len(detail == 0):
-            detect += detail
-            print(detail)
-        else:
-            for detail_item in detail:
-                # 判断是否是 NavigableString类型
-                if isinstance(detail_item, NavigableString):
-                    continue
-                detect += '\r\n' + detail_item.text
+        # 可能该标签有文字描述
+        detect += detail.text
+        # for detail_item in detail:
+        #     # 判断是否是 NavigableString类型
+        #     if isinstance(detail_item, NavigableString):
+        #         continue
+        #     detect += '\r\n' + detail_item.text
 
         download_comic(image_url, image_name, detect)
     except Exception as e:
@@ -83,7 +101,7 @@ def download_comic(comic_url, comic_name, *args, **kwargs) -> None:
     print(_path_1)
     if not os.path.exists(_path_1):
         os.makedirs(_path_1)
-    print("需要下载的链接地址是： " + comic_url)
+    # print("需要下载的链接地址是： " + comic_url)
 
     file_despath = _path_1 + '/' + comic_name + ".txt"
     # if not (os.path.exists(file_despath)):
@@ -94,7 +112,7 @@ def download_comic(comic_url, comic_name, *args, **kwargs) -> None:
     file_pth = _path_1 + "/" + comic_name + '.jpg'
     if os.path.exists(file_pth):
         return
-    response = requests.get(comic_url, headers=headers, proxies=proxies)
+    response = LockThreadGetURL(comic_url)
     with open(file_pth, 'wb') as f:
         f.write(response.content)
         print(f'<{comic_name}> 下载成功~')
@@ -107,7 +125,7 @@ def GetImage(url, exhibits):
     Secondir = path
     if not os.path.exists(path):
         os.makedirs(path)
-    res = requests.get(url, headers=headers, proxies=proxies)
+    res = LockThreadGetURL(url)
 
     soup = bs(res.text, 'lxml')
 
@@ -116,13 +134,13 @@ def GetImage(url, exhibits):
     for item in parent:
         path = item.select('.pro_img')[0]['href']
         image_url = f'{host}{path}'
-        print("GetImage   " + image_url)
+        print("\r\n GetImage   " + image_url)
         DownImage(image_url)
 
 
 # 获取每个展品类的URL--------- 1
 def GetURL():
-    res = requests.get(URL, headers=headers, proxies=proxies)
+    res = LockThreadGetURL(URL)
     soup = bs(res.text, 'lxml')
     list_item = soup.select('div .product')
 
@@ -144,7 +162,7 @@ def GetURL():
 
 # 解析展品每页的url--------- 2
 def GetEveryPage(url, exhibits):
-    res = requests.get(host + url, headers=headers, proxies=proxies)
+    res = LockThreadGetURL(host + url)
     temp = re.findall(".queryArticleByCondition\(this,'(.*?\.html)'\)\" class", res.text)
     temp.append(url)
     for item in temp:
